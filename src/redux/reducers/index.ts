c4af2a5e8ba13a8c06db8002a82b7adbe26b7410
Matheus Roque <matheusfariasroq@gmail.com>
2022-02-09 22:35:32 -0300

    feat: add index reducer

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { products } from './products';
+import { cart } from './cart';
+
+export const reducers = combineReducers({
+  products,
+  cart
+});
