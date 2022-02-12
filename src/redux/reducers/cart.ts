5db9ef09b8acb75e8c822dd9631f730977e87c4b
Matheus Roque <matheusfariasroq@gmail.com>
2022-02-12 13:33:21 -0300

    feat: create cart reducer

--- src/redux/reducers/cart.ts
+++ src/redux/reducers/cart.ts
@@ -1,5 +1,5 @@
 import { AnyAction } from 'redux'
-import * as types from '../reduxTypes/cart.types';
+import * as consts from '../consts/cart.consts';
 export type Cart = {
   data: {
     product: {
@@ -8,28 +8,63 @@ export type Cart = {
       price: number;
       category_id: number;
       description: string;
-    }
+    },
+    images: {
+      id: string;
+      product_id: string;
+      filename: string;
+      original_filename: string;
+      url: string;
+    }[]
+    quantity: number;
   }[],
 }
 
 const initialState: Cart = {
-  data: []
+  data: [],
 }
 export function cart(state = initialState, action: AnyAction) {
-  console.log(action.payload)
+  console.log(action)
   switch (action.type) {
-    case types.CART_ADD:
+    case consts.CART_ADD: {
       return {
         ...state,
         data: [...state.data, action.payload],
-
       }
-    case types.CART_REMOVE:
+    }
+    case consts.CART_REMOVE: {
       const filtered = state.data.filter(d => d.product.id !== action.payload.id)
       return {
         ...state,
         data: filtered
       }
+    }
+    case consts.CART_INCREMENT: {
+      const cart = state.data.filter(d => {
+        if (d.product.id === action.payload.productId) {
+          d.quantity += 1;
+        }
+        return d;
+      });
+      return {
+        ...state,
+        data: [...cart]
+      }
+
+    }
+    case consts.CART_DECREMENT: {
+      const cart = state.data.filter(d => {
+        if (d.product.id === action.payload.productId && d.quantity > 0) {
+          d.quantity -= 1;
+        }
+        return d.quantity !== 0 ? d : null;
+      });
+      return {
+        ...state,
+        data: [...cart]
+      }
+    }
+
     default:
       return state;
   }
