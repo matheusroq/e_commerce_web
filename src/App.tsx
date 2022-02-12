b89de29809017013598fa304606379d24ef2b602
Matheus Roque <matheusfariasroq@gmail.com>
2022-02-12 13:35:29 -0300

    feat: add routes provider and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Home } from './pages/Home';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux';
+import { ERoutes } from './routes';
 function App() {
   return (
     <>
       <Provider store={store}>
-        <Home />
+        <BrowserRouter>
+          <ERoutes />
+        </BrowserRouter>
       </Provider>
     </>
   );
